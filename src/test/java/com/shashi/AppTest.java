61a031bce898d66a1fb3bc15cd92e98a28696956
shashikumar <shashik064@gmail.com>
2023-03-29 22:35:36 +0530

    my frist commit

---
+++ src/test/java/com/shashi/AppTest.java
@@ -0,0 +1,15 @@
+package com.shashi;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+   
+    public void shouldAnswerWithTrue()
+    {
+       System.out.println("hello"); 
+    }
+}
